0897a2ec46c9c75d30f9ed6f1495c95ba13bf25a
MouhsineNejmi <nejmimouhsine@gmail.com>
2024-07-26 15:59:30 +0100

    setup eslint and prettier in admin and front office

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
+  const a = 1;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Admin Panel</h1>
