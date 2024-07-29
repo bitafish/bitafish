cdff0539b35b5d8403c7ed247012e9328c86e8fe
Apple <Apple@MacBook-Air.local>
2024-07-29 14:53:20 -0700

    push to ci-cd branch

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
+import Image from "next/image"
 
-const Home = () => {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Admin Panel</h1>
     </main>
   );
-};
-
-export default Home;
+}
