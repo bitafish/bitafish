1e13dfedbb453188535d1558cdc38db9e1f1ca38
Mouhsine Nejmi <97952181+MouhsineNejmi@users.noreply.github.com>
2024-07-31 05:57:56 -0700

    Setup nx workspace (#3)
    * replace vue with nextjs and configure eslint and prettier
    
    * setup ci workflow
    
    * change title in home page
    
    * setup eslint and prettier in admin and front office
    
    * pushing with prettier errors
    
    * push to ci-cd branch
    
    * test husky
    
    * something
    
    * setup project workspace using nx monorepo
    
    ---------
    
    Co-authored-by: Apple <Apple@MacBook-Air.local>

--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express, { Express } from "express";
-
-const port = 4000;
-const app: Express = express();
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
