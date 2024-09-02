4f7e035cfa376bf7695ab5d2e5d769550f3578ff
MouhsineNejmi <nejmimouhsine@gmail.com>
2024-09-02 16:47:05 +0100

    fix-conflicts/products-api

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -4,7 +4,6 @@ import usersRoutes from './user.route';
 import authRoutes from './auth.route';
 import productsRoutes from './product.route'
 import categoriesRoutes from './category.route'
-import productRoutes from './products.route';
 import reviewsRoutes from './reviews.route';
 import ordersRoutes from './order.routes';
 
@@ -14,7 +13,6 @@ router.use('/users', usersRoutes);
 router.use('/auth', authRoutes);
 router.use('/products',productsRoutes);
 router.use('/categories', categoriesRoutes);
-router.use('/products', productRoutes);
 router.use('/reviews', reviewsRoutes);
 router.use('/orders', ordersRoutes);
 
