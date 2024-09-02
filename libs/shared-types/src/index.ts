9c385ca171f33cd4d6e393a339819e57f784b948
bitafish <bitafish.dev@gmail.com>
2024-09-02 08:42:34 -0700

    Ben/products api (#12)
    * categories-products
    
    * reviews
    
    * remove product created to test with real api
    
    ---------
    
    Co-authored-by: mohamed benchekroun <mohamedbckn01@gmail.com>
    Co-authored-by: MouhsineNejmi <nejmimouhsine@gmail.com>

--- libs/shared-types/src/index.ts
+++ libs/shared-types/src/index.ts
@@ -3,4 +3,4 @@ export * from './types/review';
 export * from './types/product';
 export * from './types/category';
 export * from './types/order';
-export * from './types/order-item'
+export * from './types/order-item';
